ccfd7b9975e8ed5012d49c6d6dcab674e1197559
tharudaya <tharudaya94@gmail.com>
2025-03-22 15:46:04 +0530

    virtual dom explain

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
-import { useState } from "react";
 import Child1 from "./components/child1";
-import Child2 from "./components/child2";
-
 
 function App() {
-  const [isChildChanged, setIsChildChanged] = useState(false);
-
-  const changeChild = () => {
-    setIsChildChanged(!isChildChanged)
-  }
-
   return (
-    <>
-      <h1>Hey I am Parent - SL Frontend Developer</h1>
-      <button onClick={changeChild}>Change Component</button>
-      {isChildChanged ? <Child1 /> : <Child2 />}
-    </>
+    <div>
+      <h1>SL Frontend Developer</h1>
+      <Child1 />
+    </div>
   )
 }
 
--- src/components/child1.jsx
+++ src/components/child1.jsx
@@ -1,32 +1,18 @@
-import { useEffect, useState } from "react";
+/* eslint-disable react/prop-types */
+import { useState } from "react";
 
 function Child1() {
-
     const [count, setCount] = useState(0);
-
-    useEffect(() => {
-        console.log("child 1 mounted");
-    }, []);
-
-    useEffect(() => {
-        console.log("child 1 updated with count ", count);
-    }, [count]);
-
-    useEffect(() => {
-        return () => {
-            console.log("child 1 unmounted!");
-        };
-    }, []);
 
     const handleClick = () => {
         setCount(count + 1);
     };
 
     return (
-        <>
-            <h2>child1</h2>
-            <button onClick={handleClick}>Update</button>
-        </>
+        <div>
+            <p>Count is {count}</p>
+            <button onClick={handleClick}>update</button>
+        </div>
     )
 }
 
